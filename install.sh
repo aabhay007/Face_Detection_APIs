2238ab362e2cf56b5b6c328190dc8676a26c9847
aabhay007 <abhay007official@gmail.com>
2025-01-07 14:15:54 +0530

    aaa

--- install.sh
+++ install.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
 
-# Exit on any error
-set -e
 
-# Use a precompiled CMake binary
-CMAKE_VERSION="3.27.0"  # Replace with your desired version
-
-echo "Downloading CMake version $CMAKE_VERSION..."
-
-
-echo "Extracting CMake..."
-tar -xvzf cmake-$CMAKE_VERSION-linux-x86_64.tar.gz
-
-echo "Installing CMake..."
-mv cmake-$CMAKE_VERSION-linux-x86_64 /opt/cmake
-ln -s /opt/cmake/bin/* /usr/local/bin/
-
-# Verify installation
-echo "Verifying CMake installation..."
-cmake --version
-
-echo "CMake $CMAKE_VERSION installed successfully!"
